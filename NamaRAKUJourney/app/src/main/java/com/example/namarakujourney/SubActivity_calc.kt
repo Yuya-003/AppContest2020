7db0fdadf93aa30f129cc1771e3d4e593e15edd1
PERSON <1203person@gmail.com>
2020-02-25 04:16:48 +0900

    会計機能の内部数値処理の実装

--- NamaRAKUJourney/app/src/main/java/com/example/namarakujourney/SubActivity_calc.kt
+++ NamaRAKUJourney/app/src/main/java/com/example/namarakujourney/SubActivity_calc.kt
@@ -1,9 +1,10 @@
-package com.example.namarakujourney
+package com.example.appcontest_p
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import com.example.namarakujourney.R
 import kotlinx.android.synthetic.main.activity_sub_calc.*
 
 class SubActivity_calc : AppCompatActivity() {
@@ -16,15 +17,11 @@ class SubActivity_calc : AppCompatActivity() {
 
         editYenInput.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-                print("入力前")
-                textViewTotal.setText("入力中してください")
             }
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                print("入力中")
-                textViewTotal.setText("入力中...")
             }
             override fun afterTextChanged(s: Editable?) {
-                print("入力後")
+                //println("入力後")
                 var pr_10000: Int = 0
                 var pr__5000: Int = 0
                 var pr__1000: Int = 0
@@ -36,47 +33,51 @@ class SubActivity_calc : AppCompatActivity() {
                 var pr_____1: Int = 0
 
                 print(s.toString())
-                if (intPurchaseTotal == null){
+
+                if (editYenInput == null){
                     textViewTotal.setText("0")
-                } else{
+                } else {
                     var money: Int = 0
                     money = s.toString().toInt()
+                    println(money)
                     textViewTotal.setText(s.toString())
-                    if(money > 10000) {
-                        v10000.setText((money / 10000).toString())
+                    if(money >= 10000){
+                        pr_10000 = money/10000
+                        money = money % (pr_10000*10000)
                     }
-                    while (money == 0){
-                        if(money > 10000) {
-                            pr_10000++
-                            money - 10000
-                        }else if(money > 5000){
-                            ++pr__5000
-                            money - 5000
-                        }else if(money > 1000){
-                            ++pr__1000
-                            money - 1000
-                        }else if(money > 500){
-                            ++pr___500
-                            money - 500
-                        }else if(money > 100){
-                            ++pr___100
-                            money - 100
-                        }else if(money > 50){
-                            ++pr____50
-                            money - 50
-                        }else if(money > 10){
-                            ++pr____10
-                            money - 10
-                        }else if(money > 5){
-                            ++pr_____5
-                            money - 5
-                        }else if(money > 1){
-                            pr_____1 = money
-                            money = 0
+                    if(money >= 1000){
+                        pr__1000 = money/1000
+                        money = money % (pr__1000*1000)
+                        if (pr__1000 > 5){
+                            pr__5000 = 1
+                            pr__1000 = pr__1000 - 5
+                        }
+
+                    }
+                    if(money >= 100){
+                        pr___100 = money/100
+                        money = money % (pr___100*100)
+                        if (pr___100 > 5){
+                            pr___500 = 1
+                            pr___100 = pr___100 - 5
                         }
                     }
-                    textViewTotal.setText("finished_caliculate")
-                    //v10000.setText(pr_10000.toString())
+                    if(money >= 10 ){
+                        pr____10 = money/10
+                        money = money % (pr____10*10)
+                        if (pr____10 > 5){
+                            pr____50 = 1
+                            pr____10 = pr____10 - 5
+                        }
+                    }
+                    if(money >= 1 ){
+                        pr_____1 = money/1
+                        if (pr_____1 > 5){
+                            pr_____5 = 1
+                            pr_____1 = pr_____1 - 5
+                        }
+                    }
+                    v10000.setText(pr_10000.toString())
                     v5000.setText(pr__5000.toString())
                     v1000.setText(pr__1000.toString())
                     v500.setText(pr___500.toString())
