91b295222e2fdaaa683e4c6722fb2022566d4118
PERSON <1203person@gmail.com>
2020-02-25 05:42:53 +0900

    会計機能改修-入力欄が空欄時前回の値を保存するバグを修正

--- NamaRAKUJourney/app/src/main/java/com/example/namarakujourney/SubActivity_calc.kt
+++ NamaRAKUJourney/app/src/main/java/com/example/namarakujourney/SubActivity_calc.kt
@@ -30,48 +30,49 @@ class SubActivity_calc : AppCompatActivity() {
                 var pr_____1: Int = 0
 
                 print(s.toString())
-                if (editYenInput == null){
+                if (s.toString() == ""){
                     textViewTotal.setText("0")
                 } else {
                     var money: Int = 0
                     money = s.toString().toInt()
                     println(money)
                     textViewTotal.setText(s.toString())
-                    if(money >= 10000){
-                        pr_10000 = money/10000
-                        money = money % (pr_10000*10000)
+                    if (money >= 10000) {
+                        pr_10000 = money / 10000
+                        money = money % (pr_10000 * 10000)
                     }
-                    if(money >= 1000){
-                        pr__1000 = money/1000
-                        money = money % (pr__1000*1000)
-                        if (pr__1000 >= 5){
+                    if (money >= 1000) {
+                        pr__1000 = money / 1000
+                        money = money % (pr__1000 * 1000)
+                        if (pr__1000 >= 5) {
                             pr__5000 = 1
                             pr__1000 = pr__1000 - 5
                         }
                     }
-                    if(money >= 100){
-                        pr___100 = money/100
-                        money = money % (pr___100*100)
-                        if (pr___100 >= 5){
+                    if (money >= 100) {
+                        pr___100 = money / 100
+                        money = money % (pr___100 * 100)
+                        if (pr___100 >= 5) {
                             pr___500 = 1
                             pr___100 = pr___100 - 5
                         }
                     }
-                    if(money >= 10 ){
-                        pr____10 = money/10
-                        money = money % (pr____10*10)
-                        if (pr____10 >= 5){
+                    if (money >= 10) {
+                        pr____10 = money / 10
+                        money = money % (pr____10 * 10)
+                        if (pr____10 >= 5) {
                             pr____50 = 1
                             pr____10 = pr____10 - 5
                         }
                     }
-                    if(money >= 1 ){
-                        pr_____1 = money/1
-                        if (pr_____1 >= 5){
+                    if (money >= 1) {
+                        pr_____1 = money / 1
+                        if (pr_____1 >= 5) {
                             pr_____5 = 1
                             pr_____1 = pr_____1 - 5
                         }
                     }
+                }
                     v10000.setText(pr_10000.toString())
                     v5000.setText(pr__5000.toString())
                     v1000.setText(pr__1000.toString())
@@ -81,7 +82,6 @@ class SubActivity_calc : AppCompatActivity() {
                     v10.setText(pr____10.toString())
                     v5.setText(pr_____5.toString())
                     v1.setText(pr_____1.toString())
-                }
             }
         })
     }
